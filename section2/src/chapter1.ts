290ec8429b1deb439b1aba58955e1794288d41e4
gyuseung <lgs4002@naver.com>
2023-06-03 23:36:03 +0900

    원시타입과 리터럴타입

---
+++ section2/src/chapter1.ts
@@ -0,0 +1,36 @@
+// number
+// :은 주석(annotation)
+let num1: number = 123;
+let num2: number = -123;
+let num3: number = 0.123;
+let num4: number = -0.123;
+let num5: number = Infinity;
+let num6: number = -Infinity;
+let num7: number = NaN;
+
+// string
+let str1: string = "hello";
+let str2: string = "hello";
+let str3: string = `hello`;
+let str4: string = `hello ${num1}`;
+
+// boolean
+let bool1: boolean = true;
+let bool2: boolean = false;
+
+// null
+let null1: null = null;
+
+// undefined
+let unde1: undefined = undefined;
+
+// "strictNullChecks": false <- 로 가능하게 할 수 있음
+// let numA: number = null;
+
+// 리터럴 타입
+// 리터럴 -> 값
+let numA: 10 = 10;
+
+let strA: "hello" = "hello";
+
+let boolA: true = true;
